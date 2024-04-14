445ec75634754ea2a7ef48dcbb3b8081feaba9bf
BaoDias2505 <nguyenmaugiabao2505@gmail.com>
2024-04-14 15:02:26 +0700

    updated 14/4

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FinalProject_MachineLearning
+
+- MBart là một mô hình máy học dựa trên kiến trúc Transformer, được đào tạo để xử lý nhiều ngôn ngữ cùng một lúc. Nó được huấn luyện với một loạt các ngôn ngữ và có khả năng dịch một câu từ một ngôn ngữ nguồn sang một ngôn ngữ đích mà không cần thông tin về cặp ngôn ngữ cụ thể. Trong model đã sử dụng để dịch văn bản từ tiếng Anh sang tiếng Việt.
+- LangChain là một thư viện Python được tạo ra để tạo ra các câu chuyện hoặc văn bản mới dựa trên các kịch bản cụ thể. Nó sử dụng các mô hình ngôn ngữ để dự đoán và tạo ra các đoạn văn bản mới dựa trên các kịch bản được cung cấp.
+- Sử dụng HuggingFace Hub để tải xuống một mô hình ngôn ngữ đã được đào tạo và sử dụng nó để tạo ra câu chuyện từ các kịch bản đã cho.
+- Sử dụng mô hình "Salesforce/blip-image-captioning-base" chuyển đổi hình ảnh thành văn bản.
--- app.py
+++ app.py
@@ -1,14 +1,41 @@
 from transformers import pipeline
-from langchain import PromptTemplate, LLMChain, OpenAI
+from langchain import PromptTemplate, LLMChain
+from langchain import HuggingFaceHub
+
 import requests
 import os
 import streamlit as st
-from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
 from datasets import load_dataset
-import torch
-import soundfile as sf
+from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
 
-HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
+
+def translate_article_Eng_Viet(article_hi):
+    model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+    tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+
+    tokenizer.src_lang = "en_XX"
+    encoded_hi = tokenizer(article_hi, return_tensors="pt")
+    generated_tokens = model.generate(
+        **encoded_hi,
+        forced_bos_token_id=tokenizer.lang_code_to_id["vi_VN"]
+    )
+    translated_text = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+    
+    translated_text_str = " ".join(translated_text)
+    
+    return translated_text_str
+
+
+def generate_story(scenario,llm):
+    template = '''
+    CONTEXT: {scenario}
+    STORY:
+    '''
+    prompt = PromptTemplate(template=template, input_variables=["scenario"])
+    chain = LLMChain(prompt=prompt,llm=llm)
+    story = chain.predict(scenario=scenario)
+    return translate_article_Eng_Viet(story)
+
 
 def img2text(url):
     image_to_text = pipeline('image-to-text', model="Salesforce/blip-image-captioning-base")
@@ -17,40 +44,39 @@ def img2text(url):
     
     return text
 
-def text2speech(message):
-    processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
-    model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
-    vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
 
-    inputs = processor(text=message, return_tensors="pt")
-    embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
-    speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+def generate_story_from_scenario(scenario):
+    repo_id = "tiiuae/falcon-7b-instruct"
+    hf_token = "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"
+    llm = HuggingFaceHub(huggingfacehub_api_token=hf_token, repo_id=repo_id, verbose=False, model_kwargs={"temperature": 0.1, "max_new_tokens": 1500})
+    story = generate_story(scenario, llm)
+    return story
 
-    speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
-
-    sf.write("speech.wav", speech.numpy(), samplerate=16000)
-        
 
 def main():
-    
-    st.set_page_config(page_title='Image to text anhd audio')
-    st.header('Turn img into text')
-    uploaded_file = st.file_uploader('Choose an image...',type='jpg')
+    st.set_page_config(page_title='Image to text and audio')
+    st.title("🔄 Chuyển ảnh thành văn bản")
+
+    uploaded_file = st.file_uploader('🖼 Upload hình ảnh của bạn ở đây...')
     
     if uploaded_file is not None:
         bytes_data = uploaded_file.getvalue()
-        with open(uploaded_file.name,'wb') as file:
+        with open(uploaded_file.name, 'wb') as file:
             file.write(bytes_data)
-        st.image(uploaded_file,caption='Uploaded Image.',use_column_width=True)
+        st.image(uploaded_file, caption='Ảnh đã tải lên. Đang xử lý ⌛ ', use_column_width=True)
+        
         scenario = img2text(uploaded_file.name)
-        text2speech(scenario)
         
+        engtovie = translate_article_Eng_Viet(scenario)
         
-        st.write(scenario)
-        st.audio('speech.wav')
+        story = generate_story_from_scenario(scenario)
         
-        
-        
-        
+        st.write('Đã xử lý xong ✅')
+        st.write(engtovie)
+        with st.expander('📖 Tiếng Anh'):
+            st.write(scenario)
+        with st.expander('💬 Câu chuyện có thể phát triển từ mô tả' ):
+            st.write(story)
+
 if __name__ == '__main__':
     main()
